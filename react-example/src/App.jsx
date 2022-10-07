cc492b0d2fbf7b196440a088149b368403b11171
Quoc Toan <toan124677@gmail.com>
2022-10-07 14:54:19 +0700

    Practice with state in class component

--- react-example/src/App.jsx
+++ react-example/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Welcome from './components/Welcome'
 import Comment from './components/Comment'
+import Clock from './components/Clock'
 import './App.css'
 
 function App() {
 
   return (
     <div>
+      <Clock />
       <Welcome name="Toan" />
       <Comment userName="React" content="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
     </div>
