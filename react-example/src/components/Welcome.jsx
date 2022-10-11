5dc258453ef3b8c855441cf260fde894f8b6a279
Quoc Toan <toan124677@gmail.com>
2022-10-11 17:29:38 +0700

    Practice with conditional rendering

--- react-example/src/components/Welcome.jsx
+++ react-example/src/components/Welcome.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 class Welcome extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {login: false}
+  }
+
+  changeState = () => {
+    this.setState((prevState) => ({login: !prevState.login}))
+  }
+
   render() {
-    return <h1>Hello, {this.props.name}</h1>
+    let greeting = <h1>Hello, {this.props.name}</h1>
+
+    if (this.state.login) {
+      greeting = <h1>Welcome back {this.props.name}</h1>
+    }
+    return (
+      <>
+        {greeting}
+        <button onClick={this.changeState}>Change state</button>
+      </>
+    )
   }
 }
 
