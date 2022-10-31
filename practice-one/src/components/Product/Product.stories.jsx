10a89cd7dca10d503dd4cbe1dd35f6bbea3f29d1
Quoc Toan <toan124677@gmail.com>
2022-10-31 09:49:04 +0700

    Test product image with stories

--- practice-one/src/components/Product/Product.stories.jsx
+++ practice-one/src/components/Product/Product.stories.jsx
@@ -1,4 +1,5 @@
 import Product from './Product';
+import image from '../../assets/images/product.png'
 
 export default {
   title: 'Product Card',
@@ -9,6 +10,7 @@ const Template = (args) => <Product {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
+  imageURL: image,
   title: 'Beats',
   content: `Up to 8 hours of battery life
   With Fast Fuel, a 5-minute charge = 2 hours of playback`,
