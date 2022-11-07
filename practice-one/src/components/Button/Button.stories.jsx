9f70022a5881d0fa5ae1b87d83d373f62d45b4c7
Quoc Toan <toan124677@gmail.com>
2022-11-07 09:44:39 +0700

    Add default value for component

--- practice-one/src/components/Button/Button.stories.jsx
+++ practice-one/src/components/Button/Button.stories.jsx
@@ -1,18 +1,41 @@
 import Button from './Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
+  argTypes: {
+    title: {
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: 'buy now',
+      control: {
+        type: 'text',
+      },
+      description: 'The content in the button',
+    },
+    size: {
+      defaultValue: 'small',
+      description: 'Change size of the button',
+    },
+    style: {
+      defaultValue: 'dark',
+      description: 'Change color of the button',
+    },
+  },
 };
 
 const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
   style: 'dark',
 };
-
 
 export const Medium = Template.bind({});
 Medium.args = {
