c2c679e5b9e7925dc76d2dc4f83c849f59b06fc2
Quoc Toan <toan124677@gmail.com>
2022-11-14 10:05:56 +0700

    Update styling for link component

--- practice-one/src/components/Link/Link.jsx
+++ practice-one/src/components/Link/Link.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 import './link.css'
 
-export default function Link({to, children}) {
-  return <a className='link' href={to}>{children}</a>
+export default function Link({to, children, style}) {
+  return <a className={style ? `link  link--${style}` : 'link'} href={to}>{children}</a>
 }
 
 Link.propTypes = {
   to: PropTypes.string.isRequired,
   children: PropTypes.any.isRequired,
+  style: PropTypes.string,
 }
