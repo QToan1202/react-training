d60e8e8d67833ff4ad2073eac5c3a45e0b8ff552
Quoc Toan <toan124677@gmail.com>
2022-11-15 16:29:15 +0700

    Create text component

---
+++ practice-one/src/components/Text/Text.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import './text.css';
+
+export default function Text({ children, size, weight, color, leading }) {
+  const styleClass = ['text'];
+
+  leading && styleClass.push(`leading-${leading}`);
+  color && styleClass.push(`text-${color}`);
+  weight && styleClass.push(`text-${weight}`);
+  size && styleClass.push(`text-${size}`);
+
+  console.log(styleClass)
+  return <p className={styleClass.join('  ')}>{children}</p>;
+}
+
+Text.propTypes = {
+  children: PropTypes.any,
+  size: PropTypes.oneOf(['xl', '2xl','3xl', '4xl','5xl']),
+  weight: PropTypes.oneOf(['normal', 'medium', 'semibold']),
+  color: PropTypes.oneOf(['black', 'white', 'yellow-400', 'yellow-700']),
+  leading: PropTypes.oneOf(['normal', 'relaxed', 'loose', 'extra-loose']),
+};
