1221ea095430970c91e728f32b295c91d5e96c30
Quoc Toan <toan124677@gmail.com>
2022-12-29 11:37:02 +0700

    Practice with useMemo

--- react-example/src/components/Comment.jsx
+++ react-example/src/components/Comment.jsx
@@ -1,14 +1,25 @@
 import { forwardRef } from 'react'
 import reactLogo from '../assets/react.svg'
 import PropTypes from 'prop-types'
+import { useMemo } from 'react'
 
-const Comment = forwardRef((props, ref) => {
+const Comment = forwardRef(({userName, content}, ref) => {
+  const memoFn = useMemo(() => countLetter(content), [content])
+  const countLetter = (str) => {
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+      count++;
+    }
+    return count
+  }
+
   return (
     <div ref={ref} className="comment">
       <img src={reactLogo} alt="React logo" />
       <div className="comment-content">
-        <h2>{props.userName}</h2>
-        <p>{props.content}</p>
+        <h2>{userName}</h2>
+        <p>{content}</p>
+        <p>{memoFn}</p>
       </div>
     </div>
   )
