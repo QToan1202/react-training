7796d86b667dff4baba10a47124ad4502b84d654
Quoc Toan <toan124677@gmail.com>
2023-01-18 16:20:07 +0700

    Conditional render label for input

--- practice-two/src/components/Input/Input.jsx
+++ practice-two/src/components/Input/Input.jsx
@@ -1,22 +1,43 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import styles from './Input.module.css'
 
-const Input = ({ value, type, placeholder, autoComplete }) => (
-  <input className={styles.input} type={type} placeholder={placeholder} autoComplete={autoComplete} value={value} />
+const Wrapper = styled.div`
+  display: ${({ hasLabel }) => (hasLabel ? 'grid' : 'block')};
+  grid-template-columns: 100px 270px;
+  column-gap: 5px;
+`
+
+const Input = ({ value, type, placeholder, autoComplete, label, name, onChange }) => (
+  <Wrapper hasLabel={!!label}>
+    {label ? <label className={styles.label}>{label}</label> : null}
+    <input
+      className={styles.input}
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      autoComplete={autoComplete}
+      value={value}
+      onChange={onChange}
+    />
+  </Wrapper>
 )
 
 Input.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]),
   type: PropTypes.string,
+  name: PropTypes.string,
+  label: PropTypes.string,
   placeholder: PropTypes.string,
   autoComplete: PropTypes.string,
+  onChange: PropTypes.func,
 }
 
 Input.defaultProps = {
   type: 'text',
   value: '',
   placeholder: '',
-  autoComplete: 'on',
+  autoComplete: 'off',
 }
 
 export default Input
--- practice-two/src/components/Input/Input.stories.jsx
+++ practice-two/src/components/Input/Input.stories.jsx
@@ -13,4 +13,11 @@ Default.args = {}
 export const CheckBox = Template.bind({})
 CheckBox.args = {
   type: 'checkbox',
+  label: 'My checkbox',
+}
+
+export const File = Template.bind({})
+File.args = {
+  type: 'file',
+  label: 'Add a file',
 }
