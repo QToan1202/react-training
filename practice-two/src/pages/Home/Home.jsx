43703736a32b2047d8b5824002f64f467b74ef8c
Quoc Toan <toan124677@gmail.com>
2023-02-02 11:52:09 +0700

    Fetch product data

--- practice-two/src/pages/Home/Home.jsx
+++ practice-two/src/pages/Home/Home.jsx
@@ -2,10 +2,35 @@ import { Header } from '../../layouts'
 import styles from './Home.module.css'
 import logo from '../../assets/logo.svg'
 import { Input, Option, Product, SearchBar, Select, Tag } from '../../components'
+import { useId } from 'react'
+
+// API fetch support
+import useSWR from 'swr'
+import GET from '../../utils/fetcher'
+import { API_PRODUCTS } from '../../constants/url'
+
+// Notification
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import MESSAGES from '../../constants/messages'
 
 const Home = () => {
+  const { data: products, error } = useSWR(API_PRODUCTS, GET)
+  const notifyId = useId()
+  const notifyError = () => {
+    toast.error(MESSAGES.GET_DATA_FAIL, {
+      toastId: notifyId, // Prevent duplicate toast
+      position: toast.POSITION.TOP_CENTER,
+      closeButton: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      pauseOnFocusLoss: false,
+    })
+  }
+
   return (
     <>
+      <ToastContainer />
       <Header logo={logo} />
       <div className={styles.container}>
         <SearchBar />
@@ -22,9 +47,9 @@ const Home = () => {
             </Select>
           </div>
           <div>
-            <Product title="Product" description="product description" />
-            <Product title="Product" description="product description" />
-            <Product title="Product" description="product description" />
+            {!error
+              ? products.map(({ id, name, description }) => <Product key={id} title={name} description={description} />)
+              : notifyError()}
           </div>
         </div>
       </div>
