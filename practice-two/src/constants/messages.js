0e3837fe26978f2bdf048bd02b24a8a6390d542e
Quoc Toan <toan124677@gmail.com>
2023-02-07 01:51:33 +0700

    Add error messages

--- practice-two/src/constants/messages.js
+++ practice-two/src/constants/messages.js
@@ -1,5 +1,7 @@
 const MESSAGES = {
   GET_DATA_FAIL: 'Error occurred in getting data!!!',
+  REQUIRED: 'This field can not empty',
+  MAX_LENGTH: 'This field too long maximum length',
 }
 
 export default MESSAGES
