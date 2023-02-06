ea3633a055887f54d21a81bc6caa1252a568b3ee
Quoc Toan <toan124677@gmail.com>
2023-02-06 17:48:16 +0700

    Create custom hook useDebounce

---
+++ practice-two/src/hooks/useDebounce.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+
+const useDebounce = (value, delay) => {
+  const [currentValue, setCurrentValue] = useState(value)
+  useEffect(() => {
+    const delayValue = setTimeout(() => setCurrentValue(value), delay)
+
+    return clearTimeout(delayValue)
+  }, [value, delay])
+
+  return currentValue
+}
+
+export default useDebounce
