16e74902f6c203a61af19e1844cade3e76f4ff15
Quoc Toan <toan124677@gmail.com>
2023-02-07 15:54:27 +0700

    Update style and attach id for input

--- practice-two/src/components/Input/Input.jsx
+++ practice-two/src/components/Input/Input.jsx
@@ -1,26 +1,35 @@
-import { memo } from 'react'
+import { memo, useId } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import styles from './Input.module.css'
 
 const Wrapper = styled.div`
   display: ${({ hasLabel }) => (hasLabel ? 'grid' : 'block')};
-  grid-template-columns: 100px 270px;
+  grid-template-columns: 100px 1fr;
   column-gap: 5px;
 `
 
-const Input = ({ type, placeholder, autoComplete, label, register, error }) => (
-  <Wrapper hasLabel={!!label}>
-    {label ? <label className={styles.label}>{label}</label> : null}
-    <input
-      className={styles.input}
-      type={type}
-      placeholder={placeholder}
-      autoComplete={autoComplete}
-      {...register(label, error)}
-    />
-  </Wrapper>
-)
+const Input = ({ type, placeholder, autoComplete, label, register, config }) => {
+  const id = useId()
+
+  return (
+    <Wrapper hasLabel={!!label}>
+      {label ? (
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      ) : null}
+      <input
+        id={id}
+        className={styles.input}
+        type={type}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        {...register(label, config)}
+      />
+    </Wrapper>
+  )
+}
 
 Input.propTypes = {
   type: PropTypes.string,
@@ -28,7 +37,7 @@ Input.propTypes = {
   placeholder: PropTypes.string,
   autoComplete: PropTypes.string,
   register: PropTypes.func,
-  error: PropTypes.object,
+  config: PropTypes.object,
 }
 
 Input.defaultProps = {
@@ -36,6 +45,7 @@ Input.defaultProps = {
   value: '',
   placeholder: '',
   autoComplete: 'off',
+  register: () => undefined,
 }
 
 export default memo(Input)
