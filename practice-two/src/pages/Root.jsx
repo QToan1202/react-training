60705eb17798a8ff290ea2aa33274c844c8976bf
Quoc Toan <toan124677@gmail.com>
2023-02-09 04:57:31 +0700

    Add root layout

---
+++ practice-two/src/pages/Root.jsx
@@ -0,0 +1,16 @@
+import { memo } from 'react'
+import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Header } from '../layouts'
+import logo from '../assets/logo.svg'
+
+const Root = () => (
+  <>
+    <Header logo={logo} />
+    <Outlet />
+    <ToastContainer />
+  </>
+)
+
+export default memo(Root)
