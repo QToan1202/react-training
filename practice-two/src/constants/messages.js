646c0d744e63123e85692215491aa26ed77b23e6
Quoc Toan <toan124677@gmail.com>
2023-02-08 12:03:38 +0700

    Add message for deletion fail

--- practice-two/src/constants/messages.js
+++ practice-two/src/constants/messages.js
@@ -2,6 +2,7 @@ const MESSAGES = {
   GET_DATA_FAIL: 'Error occurred in getting data!!!',
   REQUIRED: 'This field can not empty',
   MAX_LENGTH: 'This field too long maximum length',
+  DELETE_FAILED: "Can't delete current product",
 }
 
 export default MESSAGES
