0a5c2b565c2601d7ccebdcb9befd8ad6420500f6
Quoc Toan <toan124677@gmail.com>
2023-02-08 12:03:08 +0700

    Update cleanup func and mutate data onChange value

--- practice-two/src/hooks/useDebounce.js
+++ practice-two/src/hooks/useDebounce.js
@@ -1,11 +1,18 @@
 import { useEffect, useState } from 'react'
+import { mutate } from 'swr'
 
 const useDebounce = (value, delay) => {
   const [currentValue, setCurrentValue] = useState(value)
-  useEffect(() => {
-    const delayValue = setTimeout(() => setCurrentValue(value), delay)
 
-    return clearTimeout(delayValue)
+  useEffect(() => {
+    const delayValue = setTimeout(() => {
+      setCurrentValue(value)
+      mutate([import.meta.env.VITE_API_PRODUCTS, currentValue])
+    }, delay)
+
+    return () => {
+      clearTimeout(delayValue)
+    }
   }, [value, delay])
 
   return currentValue
