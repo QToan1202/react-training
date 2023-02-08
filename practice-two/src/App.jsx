ab55b4a45c6cc34b64b9ffc0fb8d5d1391bd5353
Quoc Toan <toan124677@gmail.com>
2023-02-09 03:17:43 +0700

    Set up router

--- practice-two/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { Home } from './pages'
-import '../src/styles/index.css'
-
-function App() {
-  return <Home />
-}
-
-export default App
--- practice-two/src/main.jsx
+++ practice-two/src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { DetailProduct, Home } from './pages'
+import Root from './pages/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'products/:productId',
+        element: <DetailProduct />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
