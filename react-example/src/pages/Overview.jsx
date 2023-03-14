bf2ef6b0fc25e83f66e45bfe2c9ff2752bffb54d
Quoc Toan <toan124677@gmail.com>
2023-03-14 16:41:18 +0700

    Practice conditional rendering

--- react-example/src/pages/Overview.jsx
+++ react-example/src/pages/Overview.jsx
@@ -2,13 +2,30 @@ import { useCallback, useState } from 'react';
 
 const Overview = () => {
   const [value, setValue] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
 
   const handleChangeText = useCallback((e) => {
     setValue(e.target.value);
-  }, [])
+  }, []);
+
+  const handleChangeCheck = useCallback(() => {
+    setIsChecked(prevState => !prevState);
+  }, []);
 
   return (
-    <input type={'text'} value={value} onChange={handleChangeText} />
+    <>
+      <input type={'text'} value={value} onChange={handleChangeText} />
+      {value.length > 0 && <p>Your input length: {value.length}</p>}
+      <div>
+        <label> Click here</label>
+        <input
+          type={'checkbox'}
+          checked={isChecked}
+          onChange={handleChangeCheck}
+        />
+      </div>
+      <p>Above checkbox is {isChecked ? 'checked' : 'un-checked'}</p>
+    </>
   );
 };
 
