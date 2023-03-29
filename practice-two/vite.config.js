26e295d259b576c1f263078b8f20d9e60fe5d569
Toan <toan124677@gmail.com>
2023-03-29 11:04:36 +0700

    Set up alias for styles and pages folder

--- practice-two/vite.config.js
+++ practice-two/vite.config.js
@@ -14,10 +14,12 @@ export default defineConfig({
       '@assets': `${path.resolve(__dirname, './src/assets/')}`,
       '@hooks': `${path.resolve(__dirname, './src/hooks/')}`,
       '@layouts': `${path.resolve(__dirname, './src/layouts/')}`,
+      '@pages': `${path.resolve(__dirname, './src/pages/')}`,
       '@components': `${path.resolve(__dirname, './src/components/')}`,
       '@services': `${path.resolve(__dirname, './src/services/')}`,
       '@utils': `${path.resolve(__dirname, './src/utils/')}`,
       '@constants': `${path.resolve(__dirname, './src/constants/')}`,
+      '@styles': `${path.resolve(__dirname, './src/styles/')}`,
     },
   },
 })
