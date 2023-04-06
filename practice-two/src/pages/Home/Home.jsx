1ed0155f9e2b5c4582358b0d06758004c64fc344
Toan <toan124677@gmail.com>
2023-04-06 10:32:28 +0700

    Add form title props

--- practice-two/src/pages/Home/Home.jsx
+++ practice-two/src/pages/Home/Home.jsx
@@ -124,6 +124,7 @@ const Home = () => {
       )}
       {showAddDialog && (
         <ProductForm
+          title="Add Product"
           action="add"
           isShow={showAddDialog}
           onCancel={handleCloseAddForm}
@@ -132,6 +133,7 @@ const Home = () => {
       )}
       {showEditDialog && !isFetch && (
         <ProductForm
+          title="Edit Product"
           action="edit"
           defaultValues={data}
           isShow={showEditDialog}
