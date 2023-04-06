044211115b6a67ae0317d5b21c4a99711ac2f936
Toan <toan124677@gmail.com>
2023-04-06 10:32:05 +0700

    Update style

--- practice-two/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ practice-two/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -2,8 +2,10 @@ import { memo } from 'react'
 import styles from './LoadingSpinner.module.css'
 
 const LoadingSpinner = () => (
-  <div className={styles.spinner}>
-    <div className={styles.spinner__loading}></div>
+  <div className={styles.overlay}>
+    <div className={styles.spinner}>
+      <div className={styles.spinner__loading}></div>
+    </div>
   </div>
 )
 
