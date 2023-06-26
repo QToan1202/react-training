0114e2cb4617f8cfe2dcc4305e50ddeb684e3c61
Toan <toan124677@gmail.com>
2023-06-26 16:14:38 +0700

    #60: Update hook to TypeScript

--- practice-two/src/hooks/useDebounce.ts
+++ practice-two/src/hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
-const useDebounce = (value, delay) => {
-  const [currentValue, setCurrentValue] = useState(value)
+const useDebounce = (value: string, delay: number) => {
+  const [currentValue, setCurrentValue] = useState<string>(value)
 
   useEffect(() => {
     const delayValue = setTimeout(() => {
