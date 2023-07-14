24301c7bdafe7714820e2d26c6f534d43e15b3e1
Toan <toan124677@gmail.com>
2023-07-14 17:25:47 +0700

    Config admin to login page

--- react-monorepo/apps/admin/src/app/app.tsx
+++ react-monorepo/apps/admin/src/app/app.tsx
@@ -1,14 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import { LoginPage } from '@react-monorepo/shared/ui'
+import { Route, Routes } from 'react-router-dom'
 
-import NxWelcome from './nx-welcome';
-
-export function App() {
+export const App = () => {
   return (
-    <div>
-      <NxWelcome title="admin" />
-    </div>
-  );
+    <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom', duration: 3000, isClosable: true } }}>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </ChakraProvider>
+  )
 }
-
-export default App;
