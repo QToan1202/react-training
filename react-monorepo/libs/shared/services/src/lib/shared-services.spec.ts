9e852dd7e9a6a1caf0a38e210ecafd676c153b45
Toan <toan124677@gmail.com>
2023-07-14 14:34:26 +0700

    Clear un-used code

--- react-monorepo/libs/shared/services/src/lib/shared-services.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sharedServices } from './shared-services'
-
-describe('sharedServices', () => {
-  it('should work', () => {
-    expect(sharedServices()).toEqual('shared-services')
-  })
-})
