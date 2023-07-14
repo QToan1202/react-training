50e5eb575bd3e4ffca36cfa5377cdf66b5f24d13
Toan <toan124677@gmail.com>
2023-07-14 10:09:25 +0700

    Declare common constants

---
+++ react-monorepo/libs/shared/utils/src/constants/common.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  BLACK: '#000',
+  WHITE: '#fff',
+  GRAY: '#f6f6f6',
+  GRAY_100: '#edf2f7',
+  PRIMARY: '#4bc1d2',
+}
+
+export const SHADOW = {
+  BOOK_CARD: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+  FORM: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+}
