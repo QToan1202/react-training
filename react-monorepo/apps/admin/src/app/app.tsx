a565aee77e327d34183bcc4784fe0c4385e8d4af
Toan <toan124677@gmail.com>
2023-07-17 10:42:26 +0700

    Setup tanstack query for admin app

--- react-monorepo/apps/admin/src/app/app.tsx
+++ react-monorepo/apps/admin/src/app/app.tsx
@@ -1,13 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { LoginPage } from '@react-monorepo/shared/ui'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Route, Routes } from 'react-router-dom'
+
+const queryClient = new QueryClient()
 
 export const App = () => {
   return (
-    <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom', duration: 3000, isClosable: true } }}>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom', duration: 3000, isClosable: true } }}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
