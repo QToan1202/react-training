69cbd1f001617ef59fe2204951f218f9f742edc6
Toan <toan124677@gmail.com>
2023-07-19 17:34:22 +0700

    Update router for admin app

--- react-monorepo/apps/admin/src/app/app.tsx
+++ react-monorepo/apps/admin/src/app/app.tsx
@@ -1,16 +1,43 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { LoginPage } from '@react-monorepo/shared/ui'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Route, Routes } from 'react-router-dom'
+import { FiHome, FiLogOut, FiPlus } from 'react-icons/fi'
+import { shallow } from 'zustand/shallow'
+import { ChakraProvider} from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+import { BookDetail, Dashboard, LoginPage, RegisterPage } from '@react-monorepo/shared/ui'
+import { useUserStore } from '@react-monorepo/shared/stores'
+import { AdminDashboard, EditBookPage, AddBookPage, EditMemberPage } from '../pages'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 1000 * 60 * 10 },
+  },
+})
+
+const sidebarContent = [
+  { name: 'Home', icon: FiHome, href: 'dashboard' },
+  { name: 'Add book', icon: FiPlus, href: 'add-book' },
+  { name: 'Log out', icon: FiLogOut, href: '/' },
+]
 
 export const App = () => {
+  const { loginUser } = useUserStore((state) => ({ loginUser: state.loginUser }), shallow)
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom', duration: 3000, isClosable: true } }}>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          {loginUser && (
+            <Route path="/admin" element={<Dashboard sidebar={sidebarContent} />}>
+              <Route path="dashboard" element={<AdminDashboard />} />
+              <Route path="add-book" element={<AddBookPage />} />
+              <Route path="edit-book/:bookId" element={<EditBookPage />} />
+              <Route path="edit-member/:userId" element={<EditMemberPage />} />
+              <Route path="books/:bookId" element={<BookDetail />} />
+            </Route>
+          )}
         </Routes>
       </ChakraProvider>
     </QueryClientProvider>
