74ed5a0ac7802c111c6ec33f3ed681c931d44ddb
Toan <toan124677@gmail.com>
2023-07-19 17:36:45 +0700

    Create a hire quests store

--- react-monorepo/libs/shared/stores/src/index.ts
+++ react-monorepo/libs/shared/stores/src/index.ts
@@ -1,2 +1,3 @@
+export * from './hire-request'
 export * from './book'
 export * from './user'
--- react-monorepo/libs/shared/types/src/interfaces/index.ts
+++ react-monorepo/libs/shared/types/src/interfaces/index.ts
@@ -1,3 +1,4 @@
+export * from './hire-request'
 export * from './book'
 export * from './user'
 export * from './common'
