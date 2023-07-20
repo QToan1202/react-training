9b36b0049dd862d55a8a5019e78424ed42d41a68
Toan <toan124677@gmail.com>
2023-07-20 16:23:15 +0700

    Handle make a hire book request

--- react-monorepo/libs/shared/ui/src/pages/book-detail/book-detail.tsx
+++ react-monorepo/libs/shared/ui/src/pages/book-detail/book-detail.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useId, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { shallow } from 'zustand/shallow'
 import {
@@ -23,20 +23,25 @@ import {
 import { motion } from 'framer-motion'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { AxiosResponse } from 'axios'
-import { FiEdit2, FiTrash2 } from 'react-icons/fi'
+import { FiBook, FiEdit2, FiTrash2 } from 'react-icons/fi'
 
-import { useBookStore } from '@react-monorepo/shared/stores'
-import { IBook } from '@react-monorepo/shared/types'
+import { useBookStore, useHiredStore, useUserStore } from '@react-monorepo/shared/stores'
+import { IBook, IHireRequest, IUser } from '@react-monorepo/shared/types'
 import { COLORS, MESSAGES } from '@react-monorepo/shared/utils'
-import { find, remove } from '@react-monorepo/shared/services'
+import { add, edit, find, remove } from '@react-monorepo/shared/services'
 import { ConfirmDialog } from '../../components'
 
 export const BookDetail = () => {
   const { bookId } = useParams()
-  const { books, add, removeBook } = useBookStore(
-    (state) => ({ books: state.books, add: state.add, removeBook: state.remove }),
+  const { currentUser, updateUser } = useUserStore(
+    (state) => ({ currentUser: state.loginUser, updateUser: state.login }),
     shallow
   )
+  const { books, addBook, removeBook, updateBook } = useBookStore(
+    (state) => ({ books: state.books, addBook: state.add, removeBook: state.remove, updateBook: state.update }),
+    shallow
+  )
+  const { addHireRequest } = useHiredStore((state) => ({ addHireRequest: state.add }), shallow)
   const initState = useCallback(() => {
     if (!bookId) return
     const selectedBook: IBook | undefined = books.find((item) => item.id === +bookId)
@@ -53,25 +58,24 @@ export const BookDetail = () => {
 
   useEffect(() => {
     if (!data) return
-    add(data)
+    addBook(data)
     setBookData((prevState) => ({ ...prevState, ...data }))
-  }, [data, add])
+  }, [data, addBook])
 
   const toast = useToast()
-  const toastID = useId()
   const renderError = useCallback(() => {
     if (error instanceof Error)
       return toast({
-        id: toastID,
         title: error.message,
         description: MESSAGES.ERROR_REQUEST,
         status: 'error',
       })
-  }, [toast, error, toastID])
+  }, [toast, error])
   const { isOpen, onOpen, onClose } = useDisclosure()
   const navigate = useNavigate()
+
   const queryClient = useQueryClient()
-  const { mutate } = useMutation({
+  const { mutate: mutateDeleteBook } = useMutation({
     mutationFn: (variables: { path: string; id: number }): Promise<AxiosResponse['status']> =>
       remove(variables.path, variables.id),
     onError: (error: unknown) => {
@@ -97,15 +101,96 @@ export const BookDetail = () => {
     onMutate: () => onClose(),
   })
 
+  const { mutate: mutateUpdate } = useMutation({
+    mutationFn: (variables: {
+      path: string
+      id: number
+      options: Readonly<Partial<IBook | IUser>>
+    }): Promise<IBook | IUser> => edit<IBook | IUser>(variables.path, variables.id, variables.options),
+
+    onError: (error: unknown) => {
+      if (error instanceof Error)
+        toast({
+          title: error.message,
+          description: "Action can't be performed.",
+          status: 'error',
+        })
+    },
+
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(['books', 'users'])
+      if ('author' in data) {
+        updateBook(data)
+        setBookData((prevState) => ({ ...prevState, ...data }))
+      }
+      if ('email' in data) updateUser({ ...currentUser, ...data })
+    },
+  })
+
+  const updateBooksQuantity = useCallback(() => {
+    if (!bookData) return
+    mutateUpdate({
+      path: '/books',
+      id: +bookData.id,
+      options: { ...bookData, ...{ quantity: bookData.quantity - 1 } },
+    })
+  }, [mutateUpdate, bookData])
+
+  const updateUserHireRequest = useCallback(() => {
+    if (!currentUser) return
+    mutateUpdate({
+      path: '/users',
+      id: currentUser.id,
+      options: { ...currentUser, ...{ hireRequests: currentUser.hireRequests - 1 } },
+    })
+  }, [mutateUpdate, currentUser])
+
   const handleDeleteBook = useCallback(() => {
     if (!bookId) return
-    mutate({
+    mutateDeleteBook({
       path: '/books',
       id: +bookId,
     })
-  }, [bookId, mutate])
+  }, [bookId, mutateDeleteBook])
+
+  const { mutate: mutateHireBook } = useMutation({
+    mutationFn: (variables: { path: string; options: Partial<IHireRequest> }): Promise<IHireRequest> =>
+      add<IHireRequest>(variables.path, variables.options),
+    onError: (error: unknown) => {
+      if (error instanceof Error)
+        toast({
+          title: error.message,
+          description: "Action can't be performed.",
+          status: 'error',
+        })
+    },
+
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(['hire-requests'])
+      addHireRequest(data)
+      updateBooksQuantity()
+      updateUserHireRequest()
+      toast({
+        title: 'Hire book success',
+        description: `Book ${bookData?.name} have been hired successfully.`,
+        status: 'success',
+      })
+    },
+  })
 
   const handleEditBook = useCallback(() => navigate(`/admin/edit-book/${bookId}`), [navigate, bookId])
+
+  const handleHireBook = useCallback(() => {
+    if (!bookData) return
+    mutateHireBook({
+      path: '/hire-requests',
+      options: {
+        bookId: bookData.id,
+        userId: currentUser?.id,
+        borrow_date: new Date().toJSON().slice(0, 10),
+      },
+    })
+  }, [mutateHireBook, bookData, currentUser])
 
   return (
     <Grid
@@ -121,6 +206,7 @@ export const BookDetail = () => {
         <ButtonGroup mt={5}>
           <Button
             onClick={handleEditBook}
+            isDisabled={currentUser?.role !== 'admin'}
             leftIcon={<FiEdit2 />}
             variant="outline"
             _hover={{
@@ -132,6 +218,7 @@ export const BookDetail = () => {
           </Button>
           <Button
             onClick={onOpen}
+            isDisabled={currentUser?.role !== 'admin'}
             leftIcon={<FiTrash2 />}
             variant="outline"
             _hover={{
@@ -140,6 +227,17 @@ export const BookDetail = () => {
             }}
           >
             Delete
+          </Button>
+          <Button
+            onClick={handleHireBook}
+            leftIcon={<FiBook />}
+            variant="outline"
+            _hover={{
+              bgColor: COLORS.GREEN_100,
+              color: COLORS.GREEN,
+            }}
+          >
+            Hire
           </Button>
         </ButtonGroup>
       </GridItem>
