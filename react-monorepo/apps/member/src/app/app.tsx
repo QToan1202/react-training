8887b79f71403e8f3e0c37a46c485713f8ce82d2
Toan <toan124677@gmail.com>
2023-07-20 16:21:30 +0700

    Update entry files in member app

--- react-monorepo/apps/member/src/app/app.tsx
+++ react-monorepo/apps/member/src/app/app.tsx
@@ -1,14 +1,54 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { FiHome, FiLogOut } from 'react-icons/fi'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { shallow } from 'zustand/shallow'
 
-import NxWelcome from './nx-welcome';
+import { useUserStore } from '@react-monorepo/shared/stores'
+import { BookDetail, Dashboard, LoginPage, RegisterPage } from '@react-monorepo/shared/ui'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Home } from '../pages'
 
-export function App() {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 1000 * 60 * 10 },
+  },
+})
+
+const sidebarContent = [
+  { name: 'Home', icon: FiHome, href: '/' },
+  { name: 'Log out', icon: FiLogOut, href: '#' },
+]
+
+export const App = () => {
+  const { loginUser } = useUserStore((state) => ({ loginUser: state.loginUser }), shallow)
+
   return (
-    <div>
-      <NxWelcome title="member" />
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom', duration: 3000, isClosable: true } }}>
+        <Routes>
+          {loginUser
+          ? <Route path="/*" element={<PrivateRoutes />} />
+          : <Route path="/*" element={<PublicRoutes />} />}
+        </Routes>
+      </ChakraProvider>
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+const PublicRoutes = () => (
+  <Routes>
+    <Route path="login" element={<LoginPage />} />
+    <Route path="register" element={<RegisterPage />} />
+    <Route path="*" element={<Navigate to="login" />} />
+  </Routes>
+)
+
+const PrivateRoutes = () => (
+  <Routes>
+    <Route path="/*" element={<Dashboard sidebar={sidebarContent} />}>
+      <Route path="home" element={<Home />} />
+      <Route path="books/:bookId" element={<BookDetail />} />
+      <Route path="/*" element={<Navigate to="/home" />} />
+    </Route>
+  </Routes>
+)
