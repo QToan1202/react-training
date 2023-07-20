d2c09420452cfffac9430f871934963a37fcf831
Toan <toan124677@gmail.com>
2023-07-20 16:22:32 +0700

    Update fallbackImage prop in card

--- react-monorepo/libs/shared/ui/src/components/card/card.tsx
+++ react-monorepo/libs/shared/ui/src/components/card/card.tsx
@@ -4,6 +4,7 @@ import { AspectRatio, Box, Image, Text } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 
 import { COLORS, SHADOW } from '@react-monorepo/shared/utils'
+import placeholderImage from '../../../assets/images/placeholder.png'
 
 export interface CardProps {
   href: string
@@ -14,38 +15,36 @@ export interface CardProps {
   fallbackImage?: string
 }
 
-export const Card = memo(
-  ({ href, imageUrl, name, author, alt, fallbackImage = '../../../assets/images/placeholder.png' }: CardProps) => {
-    return (
-      <Box as={Link} to={href} margin={5} flexDir="column" alignItems="center">
-        <AspectRatio ratio={2 / 3}>
-          <Image
-            objectFit="cover"
-            src={imageUrl}
-            fallbackSrc={fallbackImage}
-            alt={alt}
-            borderRadius="lg"
-            as={motion.img}
-            whileHover={{
-              y: -10,
-              filter: 'brightness(70%)',
-              backgroundColor: COLORS.BLACK,
-              boxShadow: SHADOW.BOOK_CARD,
-            }}
-            whileTap={{
-              scale: 1.1,
-            }}
-          />
-        </AspectRatio>
-        <Box p={3} textAlign="center" textTransform="capitalize">
-          <Text fontWeight="bold" noOfLines={2}>
-            {name}
-          </Text>
-          <Text color="GrayText" noOfLines={2}>
-            {author}
-          </Text>
-        </Box>
+export const Card = memo(({ href, imageUrl, name, author, alt, fallbackImage = placeholderImage }: CardProps) => {
+  return (
+    <Box as={Link} to={href} margin={5} flexDir="column" alignItems="center">
+      <AspectRatio ratio={2 / 3}>
+        <Image
+          objectFit="cover"
+          src={imageUrl}
+          fallbackSrc={fallbackImage}
+          alt={alt}
+          borderRadius="lg"
+          as={motion.img}
+          whileHover={{
+            y: -10,
+            filter: 'brightness(70%)',
+            backgroundColor: COLORS.BLACK,
+            boxShadow: SHADOW.BOOK_CARD,
+          }}
+          whileTap={{
+            scale: 1.1,
+          }}
+        />
+      </AspectRatio>
+      <Box p={3} textAlign="center" textTransform="capitalize">
+        <Text fontWeight="bold" noOfLines={2}>
+          {name}
+        </Text>
+        <Text color="GrayText" noOfLines={2}>
+          {author}
+        </Text>
       </Box>
-    )
-  }
-)
+    </Box>
+  )
+})
