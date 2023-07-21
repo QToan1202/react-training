119bb93ed48290c7872c44bf4ff86467f44234d0
Toan <toan124677@gmail.com>
2023-07-21 11:16:55 +0700

    Update fallback routes

--- react-monorepo/apps/admin/src/app/app.tsx
+++ react-monorepo/apps/admin/src/app/app.tsx
@@ -1,5 +1,6 @@
-import { Route, Routes } from 'react-router-dom'
-import { FiHome, FiLogOut, FiPlus } from 'react-icons/fi'
+import { useMemo } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { FiHome, FiPlus } from 'react-icons/fi'
 import { shallow } from 'zustand/shallow'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -7,18 +8,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BookDetail, Dashboard, LoginPage, RegisterPage } from '@react-monorepo/shared/ui'
 import { useUserStore } from '@react-monorepo/shared/stores'
 import { AdminDashboard, EditBookPage, AddBookPage, EditMemberPage } from '../pages'
+import { ISideBarItem } from '@react-monorepo/shared/types'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: { staleTime: 1000 * 60 * 10 },
   },
 })
-
-const sidebarContent = [
-  { name: 'Home', icon: FiHome, href: 'dashboard' },
-  { name: 'Add book', icon: FiPlus, href: 'add-book' },
-  { name: 'Log out', icon: FiLogOut, href: '/' },
-]
 
 export const App = () => {
   const { loginUser } = useUserStore((state) => ({ loginUser: state.loginUser }), shallow)
@@ -27,9 +23,7 @@ export const App = () => {
     <QueryClientProvider client={queryClient}>
       <ChakraProvider toastOptions={{ defaultOptions: { position: 'bottom', duration: 3000, isClosable: true } }}>
         <Routes>
-        {loginUser
-          ? <Route path="/*" element={<PrivateRoutes />} />
-          : <Route path="/*" element={<PublicRoutes />} />}
+          {loginUser ? <Route path="/*" element={<PrivateRoutes />} /> : <Route path="/*" element={<PublicRoutes />} />}
         </Routes>
       </ChakraProvider>
     </QueryClientProvider>
@@ -40,17 +34,29 @@ const PublicRoutes = () => (
   <Routes>
     <Route path="/login" element={<LoginPage />} />
     <Route path="/register" element={<RegisterPage />} />
+    <Route path="*" element={<Navigate to="login" />} />
   </Routes>
 )
 
-const PrivateRoutes = () => (
-  <Routes>
-    <Route path="/admin" element={<Dashboard sidebar={sidebarContent} />}>
-      <Route path="dashboard" element={<AdminDashboard />} />
-      <Route path="add-book" element={<AddBookPage />} />
-      <Route path="edit-book/:bookId" element={<EditBookPage />} />
-      <Route path="edit-member/:userId" element={<EditMemberPage />} />
-      <Route path="books/:bookId" element={<BookDetail />} />
-    </Route>
-  </Routes>
-)
+const PrivateRoutes = () => {
+  const sidebarContent: ISideBarItem[] = useMemo(
+    () => [
+      { name: 'Home', icon: FiHome, href: 'dashboard' },
+      { name: 'Add book', icon: FiPlus, href: 'add-book' },
+    ],
+    []
+  )
+
+  return (
+    <Routes>
+      <Route path="/admin" element={<Dashboard sidebar={sidebarContent} />}>
+        <Route path="dashboard" element={<AdminDashboard />} />
+        <Route path="add-book" element={<AddBookPage />} />
+        <Route path="edit-book/:bookId" element={<EditBookPage />} />
+        <Route path="edit-member/:userId" element={<EditMemberPage />} />
+        <Route path="books/:bookId" element={<BookDetail />} />
+      </Route>
+      <Route path="*" element={<Navigate to="admin/dashboard" />} />
+    </Routes>
+  )
+}
