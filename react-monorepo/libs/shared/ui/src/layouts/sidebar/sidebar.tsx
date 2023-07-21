6f4510253be96a177f16ec53bce0a1a878657f46
Toan <toan124677@gmail.com>
2023-07-21 11:22:12 +0700

    Update logo and style in sidebar

--- react-monorepo/libs/shared/ui/src/layouts/sidebar/sidebar.tsx
+++ react-monorepo/libs/shared/ui/src/layouts/sidebar/sidebar.tsx
@@ -1,4 +1,5 @@
 import { memo, useCallback } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 import { AspectRatio, Box, BoxProps, Button, CloseButton, Flex, Link, Image, useToast } from '@chakra-ui/react'
 import { FiLogOut } from 'react-icons/fi'
 import { shallow } from 'zustand/shallow'
@@ -7,6 +8,7 @@ import { useBookStore, useHiredStore, useUserStore } from '@react-monorepo/share
 import { COLORS } from '@react-monorepo/shared/utils'
 import { ISideBarItem } from '@react-monorepo/shared/types'
 import { SidebarItem } from '../../components'
+import logo from '../../../assets/images/library-logo.png'
 
 export interface SidebarProps extends BoxProps {
   onClose: () => void
@@ -27,7 +29,32 @@ export const Sidebar = memo(({ onClose, items, ...rest }: SidebarProps) => {
       status: 'success',
     })
   }, [logOut, toast])
+
   return (
+    <Flex
+      flexDir="column"
+      justifyContent="space-between"
+      pos="fixed"
+      zIndex={3}
+      py={5}
+      bg={COLORS.GRAY}
+      minW={{ base: 'full', md: 60 }}
+      minH="full"
+      {...rest}
+    >
+      <Box>
+        <Flex h="20" alignItems="center" mx="7" justifyContent="space-between">
+          <Link as={RouterLink} to="/">
+            <AspectRatio ratio={2.8 / 1} minW="140px">
+              <Image objectFit="cover" src={logo} />
+            </AspectRatio>
+          </Link>
+          <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+        </Flex>
+        {items.map((item: ISideBarItem) => (
+          <SidebarItem key={item.name} icon={item.icon} title={item.name} to={item.href} />
+        ))}
+      </Box>
       <Button
         onClick={handleLogOut}
         leftIcon={<FiLogOut />}
@@ -43,5 +70,6 @@ export const Sidebar = memo(({ onClose, items, ...rest }: SidebarProps) => {
       >
         Log Out
       </Button>
+    </Flex>
   )
 })
