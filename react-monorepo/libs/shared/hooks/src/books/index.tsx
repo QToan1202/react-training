4f321270c5dff184f3649085439335936324c8f1
Toan <toan124677@gmail.com>
2023-07-24 17:31:22 +0700

    Create useGetBookDetail hook

--- react-monorepo/libs/shared/hooks/src/books/index.tsx
+++ react-monorepo/libs/shared/hooks/src/books/index.tsx
@@ -1,12 +1,44 @@
-import { get } from '@react-monorepo/shared/services'
-import { IBook } from '@react-monorepo/shared/types'
-import { useQuery } from '@tanstack/react-query'
+import { AxiosResponse } from 'axios'
+import { shallow } from 'zustand/shallow'
+import { UseQueryResult, useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
-export const useGetBooks = () => {
+import { find, get, remove } from '@react-monorepo/shared/services'
+import { IBook } from '@react-monorepo/shared/types'
+import { useBookStore } from '@react-monorepo/shared/stores'
+
+export const useGetBooks = (): UseQueryResult<IBook[], unknown> => {
   const bookQuery = useQuery({
     queryKey: ['books'],
     queryFn: (): Promise<IBook[]> => get<IBook>('/books'),
   })
 
   return bookQuery
+}
+
+export const useGetBookDetail = (bookId: number | string | undefined): UseQueryResult<IBook, unknown> => {
+  const bookQuery = useQuery({
+    queryKey: ['books', bookId],
+    queryFn: (): Promise<IBook> => find<IBook>(`/books/${bookId}`),
+  })
+
+  return bookQuery
+}
+
+export const useMutateDeleteBook = (onClose: () => void) => {
+  const { removeBook } = useBookStore((state) => ({ removeBook: state.remove }), shallow)
+  const queryClient = useQueryClient()
+
+  const mutateBook = useMutation({
+    mutationFn: (variables: { path: string; id: number }): Promise<AxiosResponse['status']> =>
+      remove(variables.path, variables.id),
+
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries(['books'])
+      removeBook(variables.id)
+    },
+
+    onMutate: () => onClose(),
+  })
+
+  return mutateBook
 }
