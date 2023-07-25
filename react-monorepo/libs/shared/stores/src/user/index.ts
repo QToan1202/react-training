b6bcff80b9a86dd6c14f89f79450553c0753975e
Toan <toan124677@gmail.com>
2023-07-25 14:05:04 +0700

    Split user store with auth store

--- react-monorepo/libs/shared/stores/src/user/index.ts
+++ react-monorepo/libs/shared/stores/src/user/index.ts
@@ -1,59 +1,43 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
 
 import { IUser } from '@react-monorepo/shared/types'
 
 interface UserState {
-  loginUser?: IUser
   users: IUser[]
   add: (user: IUser) => void
   update: (user: IUser) => void
   remove: (id: number) => void
   reset: () => void
-  login: (user: IUser) => void
-  logout: () => void
 }
 
-export const useUserStore = create<UserState>()(
-  persist(
-    (set) => ({
-      loginUser: undefined,
-      users: [],
+export const useUserStore = create<UserState>()((set) => ({
+  users: [],
 
-      add: (user: IUser) => {
-        set((state) => {
-          return { users: [...state.users, user] }
-        })
-      },
+  add: (user: IUser) => {
+    set((state) => {
+      return { users: [...state.users, user] }
+    })
+  },
 
-      update: (user: IUser) => {
-        set((state) => {
-          const afterUpdate: IUser[] = state.users.map((item) => {
-            if (item.id === user.id) return user
+  update: (user: IUser) => {
+    set((state) => {
+      const afterUpdate: IUser[] = state.users.map((item) => {
+        if (item.id === user.id) return user
 
-            return item
-          })
+        return item
+      })
 
-          return { users: afterUpdate }
-        })
-      },
+      return { users: afterUpdate }
+    })
+  },
 
-      remove: (id: number) => {
-        set((state) => {
-          const afterDelete: IUser[] = state.users.filter((item) => item.id !== id)
+  remove: (id: number) => {
+    set((state) => {
+      const afterDelete: IUser[] = state.users.filter((item) => item.id !== id)
 
-          return { users: afterDelete }
-        })
-      },
+      return { users: afterDelete }
+    })
+  },
 
-      reset: () => set({ users: [] }),
-
-      login: (user: IUser) => set(() => ({ loginUser: user })),
-
-      logout: () => set(() => ({ loginUser: undefined })),
-    }),
-    {
-      name: 'user-store',
-    }
-  )
-)
+  reset: () => set({ users: [] }),
+}))
